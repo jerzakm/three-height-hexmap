ffa28dc8f03fb3a3fa77a597d24501d2c63b64e4
Marcin <jerzakm@gmail.com>
2020-07-13 20:22:55 +0200

    clamp and lerp

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,3 +1,11 @@
 export const degToRad = (degrees: number) => {
   return degrees * (Math.PI / 180)
 }
+
+export const clamp = (value: number, min: number, max: number) => {
+  return value < min ? min : value > max ? max : value
+}
+
+export const inverseLerp = (a: number, b: number, value: number) => {
+  return (clamp(value, Math.min(a, b), Math.max(a, b)) - a) / (b - a)
+}
