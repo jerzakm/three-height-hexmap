dcfa6dbd0cd184fe1369e00d93d275b3aedd589f
Marcin <jerzakm@gmail.com>
2020-07-14 23:30:35 +0200

    cleanup

--- src/three.ts
+++ src/three.ts
@@ -2,17 +2,10 @@ import {
   WebGLRenderer,
   Scene,
   PerspectiveCamera,
-  TextureLoader,
   BasicShadowMap,
-  MeshPhongMaterial,
-  NearestFilter,
-  RepeatWrapping,
   GammaEncoding,
-  MeshToonMaterial,
 } from 'three'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { calcHexLocation } from './hexGrid/hexMath'
 
 export const renderer = new WebGLRenderer({ antialias: true })
 renderer.shadowMap.enabled = true
@@ -34,9 +27,6 @@ export const camera = new PerspectiveCamera(
 camera.position.set(20, 50, 20) // all components equal
 camera.lookAt(mainScene.position) // or the origin
 
-const objLoader = new OBJLoader()
-const textureLoader = new TextureLoader()
-
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.target.set(-1, 1, -0.5)
 controls.update()
@@ -47,43 +37,6 @@ export const initScene = () => {
 
   window.addEventListener('resize', onWindowResize, false)
   onWindowResize()
-  // loadHex(terrainCanvas)
-}
-
-function loadHex(terrainCanvas: HTMLCanvasElement) {
-  objLoader.load('hexagon.obj', (group) => {
-    const texture = textureLoader.load('hexagon.png')
-    texture.magFilter = NearestFilter
-    texture.wrapS = RepeatWrapping
-    const hexagon = group.children[0]
-
-    const ctx = terrainCanvas.getContext('2d')
-
-    if (ctx) {
-      for (let x = 0; x < terrainCanvas.width; x++) {
-        for (let y = 0; y < terrainCanvas.height; y++) {
-          const height = ctx.getImageData(x, y, 1, 1).data[0] / 7 - 18
-
-          // if (height > 0) {
-          const r = 1
-          const location = calcHexLocation(x, y, r, r * Math.sqrt(3), false)
-          const newHex = hexagon.clone()
-          newHex.castShadow = true
-          newHex.receiveShadow = true
-          newHex.position.set(location.x - 50, height - 5, location.y - 50)
-
-          const material = new MeshPhongMaterial({
-            color: '#FFFFFF',
-          })
-          //@ts-ignore
-          newHex.material = material
-
-          mainScene.add(newHex)
-          // }
-        }
-      }
-    }
-  })
 }
 
 function onWindowResize() {
